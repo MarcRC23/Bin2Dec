4751e62372a91f44ffd930ada07962fe9524ee60
Marc <marcruizcastro66@gmail.com>
2022-05-02 15:39:20 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-function texto() {
-    var number1 = document.getElementById('number1')
-    var number2 = document.getElementById('number2')
-    var numerobi = document.getElementById('numerobi').value
-    var procesando = document.getElementById('procesando')
-    procesando.innerHTML = parseInt(numerobi, 2);
-    var ultimodigito = numerobi.charAt(numerobi.length - 1)
-    if (numerobi == '') {
-        procesando.innerHTML = ''
+function marcruiz() {
+    var mr1 = document.getElementById('mr1')
+    var mr2 = document.getElementById('mr2')
+    var numerob = document.getElementById('numerob').value
+    var procesar = document.getElementById('procesar')
+    procesar.innerHTML = parseInt(numerob, 2);
+    var ultimodigito = numerob.charAt(numerob.length - 1)
+    if (numerob == '') {
+        procesar.innerHTML = ''
     }
     if (ultimodigito == 0 || ultimodigito == 1) {
-        infonum2.innerHTML = 'Here is your decimal'
-        infonum.innerHTML = ''
+        mr2.innerHTML = 'Here is your decimal'
+        mr1.innerHTML = ''
     } else {
-        procesando.innerHTML = 'Waiting for a valid binary number...'
+        procesar.innerHTML = 'Waiting for a valid binary number'
     }
     if (ultimodigito >> 1 || ultimodigito >> 9999) {
-        infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
-        infonum2.innerHTML = ''
+        mr1.innerHTML = 'You entered a non binary digit'
+        mr2.innerHTML = ''
     }
     if (ultimodigito == '') {
-        infonum.innerHTML = ''
-        infonum2.innerHTML = ''
+        mr1.innerHTML = ''
+        mr2.innerHTML = ''
     }
 }
